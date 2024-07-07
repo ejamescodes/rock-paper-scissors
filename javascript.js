9759c49a9d8c238955c596db52da85e27de79b53
ejamescodes <47791697+ejamescodes@users.noreply.github.com>
2024-07-06 20:05:41 -0400

    add ui to rock paper scissors game

--- javascript.js
+++ javascript.js
@@ -30,30 +30,46 @@ function getHumanChoice(){
    return humChoice;
 }
 
-function playRound(){
+function playRound(humChoice){
+    let rtc = '';
     let humanScore = 0;
     let computerScore = 0;
     let compChoice = getComputerChoice();
-    let humChoice = getHumanChoice();
+    humChoice = humChoice.toLowerCase();
     if((compChoice == 'rock' && humChoice == 'scissors') ||
         (compChoice == 'paper' && humChoice == 'rock') ||
         (compChoice == 'scissors' && humChoice == 'paper')){
             computerScore++;
             compChoice = compChoice.charAt(0).toUpperCase() + compChoice.slice(1);
             humChoice = humChoice.charAt(0).toUpperCase() + humChoice.slice(1);
-            console.log(`You lose! ${compChoice} beats ${humChoice}.`)
+            rtc = (`You lose! ${compChoice} beats ${humChoice}.`)
     } else if((compChoice == 'paper' && humChoice == 'scissors') ||
         (compChoice == 'scissors' && humChoice == 'rock') ||
         (compChoice == 'rock' && humChoice == 'paper')){
             humanScore++;
             compChoice = compChoice.charAt(0).toUpperCase() + compChoice.slice(1);
             humChoice = humChoice.charAt(0).toUpperCase() + humChoice.slice(1);
-            console.log(`You win! ${humChoice} beats ${compChoice}.`)
+            rtc = (`You win! ${humChoice} beats ${compChoice}.`)
     } else {
-        console.log("It's a draw")
+        rtc = ("It's a draw");
     }
-    return [humanScore, computerScore];
+    hScore += humanScore;
+    cScore += computerScore;
+    rtc = rtc+` Humans: ${hScore} - Machines: ${cScore}`;
+
+    if(hScore == 5){
+        rtc = `Humans win! Humans: ${hScore} - Machines: ${cScore}`;
+    }
+    else if(cScore == 5){
+        rtc = `Computers win! Humans: ${hScore} - Machines: ${cScore}`;
+    }
+
+    results.textContent = rtc;
 }
+
+let hScore = 0;
+let cScore = 0;
+let rounds = 0;
 
 function playGame(){
     let humanScore = 0;
@@ -68,3 +84,24 @@ function playGame(){
     }
     console.log(`Final Score: Hum:${humanScore} - CPU:${computerScore}`)
 }
+
+const content = document.querySelector("#container");
+
+const btnRock = document.createElement("button");
+btnRock.textContent = "Rock";
+btnRock.onclick = () => playRound("Rock");
+
+const btnPaper = document.createElement("button");
+btnPaper.textContent = "Paper";
+btnPaper.onclick = () => playRound("Paper");
+
+const btnScissors = document.createElement("button");
+btnScissors.textContent = "Scissors";
+btnScissors.onclick = () => playRound("Scissors");
+
+content.appendChild(btnRock);
+content.appendChild(btnPaper);
+content.appendChild(btnScissors);
+
+const results = document.createElement("div");
+content.appendChild(results);
